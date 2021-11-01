fc8115c5f1734d850ab5db49460fee66cab62ab6
kevinnivekkevin <63487456+kevinnivekkevin@users.noreply.github.com>
2021-11-01 11:54:59 +0800

    Add files via upload

---
+++ imageAI/face_recognition.py
@@ -0,0 +1,24 @@
+import face_recognition
+import os, sys
+
+# python face_recog.py C:/users/Kevin/Desktop/psb.jpg C:/Users/Kevin/Desktop/faces
+
+targetImgPath = sys.argv[1]
+imageFolderPath = sys.argv[2]
+imageName = sys.argv[3]
+
+# print(targetImgPath, imageFolderPath)
+
+known_image = face_recognition.load_image_file(targetImgPath)
+unknown_image = face_recognition.load_image_file(imageFolderPath+'\\'+imageName)
+
+known_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([known_encoding], unknown_encoding,tolerance=0.45)
+
+if results[0] == True:
+	print('MATCH')
+else:
+	print('NIL')
+
