c6fe42cb3cd7a7026d9bc6ab15ddd9eb560ecc98
kevinnivekkevin <63487456+kevinnivekkevin@users.noreply.github.com>
2021-11-01 11:51:17 +0800

    Update detect.py
    unknown objects are not tagged as 'unknown' instead of just blank

--- imageAI/detect.py
+++ imageAI/detect.py
@@ -3,7 +3,7 @@ import os,sys,time,re
 
 img_file_name = sys.argv[1]
 tempDir = sys.argv[2]
-modelFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models/yolo-tiny.h5')
+modelFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'yolo-tiny.h5')
 
 detector = ObjectDetection()
 detector.setModelTypeAsTinyYOLOv3()
@@ -24,9 +24,12 @@ def detectObject(img_file_name):
 
 			if len(detections) > 1:
 				detectedObjects += ' '
+	
+	if len(detections) == 0:		
+		detectedObjects = 'Unknown'	
 
-    #Rename the file to object detected
 	print(detectedObjects)
+	#Rename the file to object detected
 	imageFileName = re.sub(r'.*\\', '',img_file_name)
 	os.rename(os.path.join(tempDir , img_file_name+"_output.jpg"), tempDir+imageFileName+';'+detectedObjects+'.jpg')
 
